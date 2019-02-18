6ed55790b0fd85a513a7dd2aeac75b6997f46394
ndgspn <nandangsopyan08@gmail.com>
2019-02-18 16:44:47 +0700

    first react app commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList App v1.0.o. It is part of a My First React</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
